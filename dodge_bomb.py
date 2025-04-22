143035f96eb7f547c5f5e778ad6540dd1f6df468
Kiichi <c0c240160f@edu.teu.ac.jp>
2025-04-22 16:09:52 +0900

    練習問題3,4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -26,6 +26,7 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    #こうかとん初期化
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
@@ -46,26 +47,42 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        # こうかとんRectと爆弾Rectが重なっていたら
+        if kk_rct.colliderect(bb_rct): 
+            print("Game Over")
+            return
+
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+
+        for key, mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]  # 左右方向
+                sum_mv[1] += mv[1]  # 上下方向
+
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
+        kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True): # 画面外だったら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) # 画面内に戻す
         screen.blit(kk_img,kk_rct)
         bb_rct.move_ip(vx, vy)  # 爆弾の移動
         yoko, tate = check_bound(bb_rct) #爆弾の描画
+        if not yoko:  # 左右どちらかにはみ出ていたら
+            vx *= -1
+        if not tate:  # 上下どちらかにはみ出ていたら
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
-        key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
-        kk_rct.move_ip(sum_mv)
-        screen.blit(kk_img, kk_rct)
+        screen.blit(bb_img, bb_rct)  # 爆弾の描画
         pg.display.update()
         tmr += 1
         clock.tick(50)
